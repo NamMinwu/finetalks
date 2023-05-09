d9ef7702310dafcce778b392af585a99b49fca27
NamMinwu <minwu06255@gmail.com>
2023-05-09 09:21:23 +0900

    FEAT : register

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,17 @@
+<form method="post">
+	<div>
+		<label>
+			<input name="email" type="email" placeholder="email" />
+		</label>
+		<label>
+			<input name="password" type="password" placeholder="Password" />
+		</label>
+		<label>
+			<input name="confirmPassword" type="password" placeholder="ConfirmPassword" />
+		</label>
+		<label>
+			<input name="name" type="text" placeholder="name" />
+		</label>
+		<button type="submit">register</button>
+	</div>
+</form>
